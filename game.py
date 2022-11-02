576e71c2bbcd6ccbc8ac1f9401780aac13fec644
Tyler Griffith <griffitht16@gmail.com>
2022-11-02 11:25:33 -0500

    Project Setup

---
+++ game.py
@@ -0,0 +1,13 @@
+def number_to_name(number):
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "paper"
+    elif number == 2:
+        return "scissors"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "spock"
+    else:
+        return "Enter number 0-4"
